5007099c89fd35a7f419cf8257bbb7e0f079eaea
Keith Mock <kmock@apple.com>
2016-11-14 14:12:16 -0800

    added solution to 1.4 using Javascript/hasing

--- JavaScript/AddTwoNumbers.js
+++ JavaScript/AddTwoNumbers.js
@@ -60,16 +60,19 @@ var addTwoNumbers = function(l1, l2) {
     tmp = l3;
     l1 = l1.next ? l1.next : {};
     l2 = l2.next ? l2.next : {};
-    while(l1.val || l2.val) { //while at least one list can be interated
-      tmp.next = new ListNode((l1.val ? l1.val : 0 + l2.val ? l2.val : 0 + carry) % 10); //create new linked list by starting at end
+    while(typeof l1.val !== "undefined"|| typeof l2.val !== "undefined") { //while at least one list can be interated
+      tmp.next = new ListNode(((l1.val + l2.val) + carry) % 10); //create new linked list by starting at end
       carry = (l1.val + l2.val + carry) % 10 !== l1.val + l2.val + carry ? 1 : 0;
       tmp = tmp.next;
       l1 = l1.next ? l1.next : {};
       l2 = l2.next ? l2.next : {};
     }
+    if(carry) {
+      tmp.next = new ListNode(carry);
+    }
     return l3;
 };
 
-var l1 = fillList([5]), l2 = fillList([5,6,4]);
+var l1 = fillList([6,4,5,0,4,4,9,4,1]), l2 = fillList([3,8,8,0,3,0,1,4,8]);
 
-console.log("answer length: ", addTwoNumbers(l1,l2));
+listcount(addTwoNumbers(l1,l2));
